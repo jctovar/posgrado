36000b9ae4d99418d344813e4095b059dc35a484
Juan Carlos Tovar Gomez <jctovar@ired.unam.mx>
2016-05-24 15:04:25 -0500

    teachers

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ angular.module('starter', ['ngRoute', 'ngResource', 'ngSanitize', 'ngAnimate', '
       // Configure URLs for icons specified by [set:]id.
       $mdIconProvider
           .icon('add', 'img/icons/ic_add_black_24px.svg')
-          .icon('more_vert', '/img/icons/ic_more_vert_black_24px.svg')    // Register a specific icon (by name)
+          .icon('more_vert', 'img/icons/ic_more_vert_black_24px.svg')    // Register a specific icon (by name)
           .icon('check', 'img/icons/ic_done_black_24px.svg')
+          .icon('arrow_back', 'img/icons/ic_arrow_back_black_24px.svg')
           .icon('search', 'img/icons/ic_search_black_24px.svg')
           .icon('edit', 'img/icons/ic_mode_edit_black_24px.svg')
           .icon('delete', 'img/icons/ic_delete_forever_black_24px.svg')
